296bba1703d33833e9da899face10b8aec54cd19
johnnithin08 <johnnithin08@gmail.com>
2023-08-01 11:50:57 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, } from "./components"
+import { StarsCanvas } from './components/canvas/StarsCanvas'
 
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
@@ -16,7 +17,8 @@ export const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <div className="relative z-0">
+        <Feedbacks />
+        <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ export const Navbar = () => {
                     {navLinks.map((link) => {
                         return (
                             <li key={link.id} className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
-                                <a href={`${link.id}`}>{link.title}</a>
+                                <a href={`#${link.id}`}>{link.title}</a>
                             </li>
                         )
                     })}
